f13f8e04f0f8e92c3f7d092a01a4353491c57845
Folarin Lawal <lawalfolarin6@gmail.com>
2023-02-05 01:40:14 +0100

    added mongoose and enabled CRUD functionality on api

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors = require("colors")
+const connectDB = require("./config/db")
+
 const port = process.env.PORT;
 const { errorHandler } = require("./middleware/errorMiddleware");
+
+connectDB()
 
 const goalRoutes = require("./routes/goalRoutes");
 
