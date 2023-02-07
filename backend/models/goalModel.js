9bfacd09c79f0dfe8e290b83ba444fb0de66ae8d
Folarin Lawal <lawalfolarin6@gmail.com>
2023-02-07 16:33:48 +0100

    register function completed

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const goalSchema = mongoose.Schema(
   {
-    author: {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: "User",
